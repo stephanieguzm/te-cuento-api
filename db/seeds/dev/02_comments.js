1c926ecc951027660f62b22bf38e0269696f0a8c
Stephanie Guzman <sguzman247@gmail.com>
2022-11-08 08:55:50 -0600

    Update syntax

--- db/seeds/dev/02_comments.js
+++ db/seeds/dev/02_comments.js
@@ -21,13 +21,13 @@ exports.seed = async function(knex) {
       id: 3,
       tea_id: 1,
       user_name: 'Carolina',
-      user_message: `It's ok. Very delicate and I tend to prefer a bolder tea. Something with more taste. This is so mellow it's almost like water. But I loved the look of the tea! How neat to see these paper thin beautiful leaves. You can tell a lot of work went into it.`
+      user_message: "It's ok. Very delicate and I tend to prefer a bolder tea. Something with more taste. This is so mellow it's almost like water. But I loved the look of the tea! How neat to see these paper thin beautiful leaves. You can tell a lot of work went into it."
     },
     {
       id: 4,
       tea_id: 1,
       user_name: 'Mayra',
-      user_message: `I was fascinated by the description of this tea and very pleased that I bought it and tried it. I made it in my clear glass teapot and loved seeing the leaves expand as they steeped. I forgot about the tea in the pot and when i went back, the color was darker and the tea was more delicious.`
+      user_message: "I was fascinated by the description of this tea and very pleased that I bought it and tried it. I made it in my clear glass teapot and loved seeing the leaves expand as they steeped. I forgot about the tea in the pot and when i went back, the color was darker and the tea was more delicious."
     },
     {
       id: 5,
@@ -63,13 +63,13 @@ exports.seed = async function(knex) {
       id: 10,
       tea_id: 3,
       user_name: 'Jorge',
-      user_message: `Oh man, this is amazing. This may very well be my new favorite. It is exquisite, like drinking the essence of spring. At 3 minutes, there is absolutely no bitterness (I think I prefer 2 minutes, it is a perfect, delicate, pale green elixir). I was afraid to try it because I don't like smoky flavored tea at all. But I am getting zero smoke flavor. It's nutty, buttery, green, and fabulous!`
+      user_message: "Oh man, this is amazing. This may very well be my new favorite. It is exquisite, like drinking the essence of spring. At 3 minutes, there is absolutely no bitterness (I think I prefer 2 minutes, it is a perfect, delicate, pale green elixir). I was afraid to try it because I don't like smoky flavored tea at all. But I am getting zero smoke flavor. It's nutty, buttery, green, and fabulous!"
     },
     {
       id: 11,
       tea_id: 3,
       user_name: 'Gina',
-      user_message: `A delightful light tea, perfect for our afternoon tea break. Pale yellow/gold in the cup, with no grassiness. A very light toasty smoky fruity treat. It is going on our reorder list for sure. No bitterness whatsoever.`
+      user_message: "A delightful light tea, perfect for our afternoon tea break. Pale yellow/gold in the cup, with no grassiness. A very light toasty smoky fruity treat. It is going on our reorder list for sure. No bitterness whatsoever."
     },
     {
       id: 12,
@@ -87,13 +87,13 @@ exports.seed = async function(knex) {
       id: 14,
       tea_id: 4,
       user_name: 'Penelope',
-      user_message: `Beautiful oolong with a slight mineral but smooth mouth feel. I get notes of cinnamon when I smell the dry leaves. When steeped, they open up and render a pale yellow liquor with a clean, delicate, mildly sweet flavor that holds up to multiple steeps and is perfect for gong fu brewing. Sip, sip away!`
+      user_message: "Beautiful oolong with a slight mineral but smooth mouth feel. I get notes of cinnamon when I smell the dry leaves. When steeped, they open up and render a pale yellow liquor with a clean, delicate, mildly sweet flavor that holds up to multiple steeps and is perfect for gong fu brewing. Sip, sip away!"
     },
     {
       id: 15,
       tea_id: 4,
       user_name: 'Sofia',
-      user_message: `These tightly rolled leaves brew into a rich green tea. The leaves expand and don't take much to make a flavorful cup.`
+      user_message: "These tightly rolled leaves brew into a rich green tea. The leaves expand and don't take much to make a flavorful cup."
     },
     {
       id: 16,
@@ -111,13 +111,13 @@ exports.seed = async function(knex) {
       id: 18,
       tea_id: 5,
       user_name: 'Alek',
-      user_message: `Delicate and sweet, the color of this tea is almost invisible when you first steep. However, pouring into the glass Fair cup reveals a yellow color that develops slightly as you sip a little at a time`
+      user_message: "Delicate and sweet, the color of this tea is almost invisible when you first steep. However, pouring into the glass Fair cup reveals a yellow color that develops slightly as you sip a little at a time"
     },
     {
       id: 19,
       tea_id: 5,
       user_name: 'Mateo',
-      user_message: `Very much like a white tea in it's subtle flavor and soft aroma, but close to green tea in flavor. For somebody like me who loves silver needle tea, this was quite special. Though, I d recommend it only to people who appreciate soft and subtle flavors, as if you don't like that sort of thing, you may just consider it 'weak'.`
+      user_message: "Very much like a white tea in it's subtle flavor and soft aroma, but close to green tea in flavor. For somebody like me who loves silver needle tea, this was quite special. Though, I d recommend it only to people who appreciate soft and subtle flavors, as if you don't like that sort of thing, you may just consider it 'weak'."
     },
     {
       id: 20,
@@ -135,13 +135,13 @@ exports.seed = async function(knex) {
       id:22,
       tea_id: 6,
       user_name: 'Sandy',
-      user_message: `YUM!!! I was dying to try this one, and I snagged it soon as it came back. I will be ordering more, for sure! I accidentally steeped it longer than I usually do, and there was barely any astringency. The leaves are large, golden, and toasty. The scent is malty and enticing. Forefront in the taste I get malt and honey. Not getting any cocoa, and I'm not sure what sweet potato tastes like (haven't eaten in in forever) but that in the description grabbed me. I really, really like this tea; it's like a warm hug for my soul. :)`
+      user_message: "YUM!!! I was dying to try this one, and I snagged it soon as it came back. I will be ordering more, for sure! I accidentally steeped it longer than I usually do, and there was barely any astringency. The leaves are large, golden, and toasty. The scent is malty and enticing. Forefront in the taste I get malt and honey. Not getting any cocoa, and I'm not sure what sweet potato tastes like (haven't eaten in in forever) but that in the description grabbed me. I really, really like this tea; it's like a warm hug for my soul. :)"
     },
     {
       id: 23,
       tea_id: 6,
       user_name: 'Jason',
-      user_message: `A good solid tea that has a lot of body and satisfying tea taste. It does boast of honey toastiness, cocoa, and roasted sweet potato. You will undoubtedly taste the toastiness and the sweet potato from the get go, but let it sit a minute or two and you will taste the fullness of the honey and cocoa. A true tea drinkers delight!`
+      user_message: "A good solid tea that has a lot of body and satisfying tea taste. It does boast of honey toastiness, cocoa, and roasted sweet potato. You will undoubtedly taste the toastiness and the sweet potato from the get go, but let it sit a minute or two and you will taste the fullness of the honey and cocoa. A true tea drinkers delight!"
     },
     {
       id: 24,
@@ -159,13 +159,13 @@ exports.seed = async function(knex) {
       id: 26,
       tea_id: 7,
       user_name: 'Bev',
-      user_message: `Really enjoy this. Though it was a bit hard to figure out how much to use. It's very smooth and doesn't have a significant acidity which is nice. Added the matcha honey and pea flower to it `
+      user_message: "Really enjoy this. Though it was a bit hard to figure out how much to use. It's very smooth and doesn't have a significant acidity which is nice. Added the matcha honey and pea flower to it"
     },
     {
       id: 27,
       tea_id: 7,
       user_name: 'Lupita',
-      user_message: `The smell is really honey like, with a little bit of smoke and deeply complex. The leaves are long and reedy, so put it in a big cup or strainer. The tea is a pale amber. it starts off sweet, then becomes more full-bodied and smokey, but you don't forget this is a green tea. `
+      user_message: "The smell is really honey like, with a little bit of smoke and deeply complex. The leaves are long and reedy, so put it in a big cup or strainer. The tea is a pale amber. it starts off sweet, then becomes more full-bodied and smokey, but you don't forget this is a green tea."
     },
     {
       id: 28,
@@ -183,13 +183,13 @@ exports.seed = async function(knex) {
       id: 30,
       tea_id: 8,
       user_name: 'Gabriel',
-      user_message: `This tea is a really interesting tea. The honey notes really stick out. Its pretty perplexing, it definitely took a couple of glasses for me to finally make up my mind on it. Would definitely recommend.`
+      user_message: "This tea is a really interesting tea. The honey notes really stick out. Its pretty perplexing, it definitely took a couple of glasses for me to finally make up my mind on it. Would definitely recommend."
     },
     {
       id: 31,
       tea_id: 8,
       user_name: 'Amelie',
-      user_message: `This is an amazing tea! I definitely get the honey in nose and taste. I'm not sure what 'orchid' means because there are tens of thousands of species and the flowers range from bitter to sweet to vegetal.`
+      user_message: "This is an amazing tea! I definitely get the honey in nose and taste. I'm not sure what 'orchid' means because there are tens of thousands of species and the flowers range from bitter to sweet to vegetal."
     },
     {
       id: 32,
@@ -201,13 +201,13 @@ exports.seed = async function(knex) {
       id: 33,
       tea_id: 9,
       user_name: 'Bruno',
-      user_message: `A light tingly feeling of fresh hay was noted on my tongue when the tea was hot. When warm, tongue tingles were moderate. Fresh sweet corn flavors had a slightly milky taste. A fragrant mouthfeel and long-lasting lingering sweet aftertaste was noted. This was an excellent example of a gentle, smooth and light tasting Silver Needle white tea. A second steep for 2.5 minutes was even better in tastes, A pleasant astringency was noted on the rear inside of the cheeks, just like a fine nice white wine.`
+      user_message: "A light tingly feeling of fresh hay was noted on my tongue when the tea was hot. When warm, tongue tingles were moderate. Fresh sweet corn flavors had a slightly milky taste. A fragrant mouthfeel and long-lasting lingering sweet aftertaste was noted. This was an excellent example of a gentle, smooth and light tasting Silver Needle white tea. A second steep for 2.5 minutes was even better in tastes, A pleasant astringency was noted on the rear inside of the cheeks, just like a fine nice white wine."
     },
     {
       id: 34,
       tea_id: 9,
-      user_name: 'Victoria',
-      user_message: `Silver Needle is one of my favorite teas (it's so delicate and delicious!), and I definitely enjoyed this, though it perhaps wasn't the best Silver Needle I've ever had. I used a gaiwan to steep the leaves multiple times.`
+      user_name: "Victoria",
+      user_message: "Silver Needle is one of my favorite teas (it's so delicate and delicious!), and I definitely enjoyed this, though it perhaps wasn't the best Silver Needle I've ever had. I used a gaiwan to steep the leaves multiple times."
     }
   ]);
 };
