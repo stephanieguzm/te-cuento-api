fc2d30aa69b646bc30948b1ef5bc5d8b1af757b3
Stephanie Guzman <sguzman247@gmail.com>
2022-11-07 22:55:30 -0600

    Add migrations table

---
+++ db/migrations/20221108033952_initial.js
@@ -0,0 +1,39 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('teas', table => {
+      table.increments('id').primary()
+      table.string('type')
+      table.string('name')
+      table.string('img')
+      table.string('origin')
+      table.string('description')
+      table.string('brew_time')
+      table.integer('temperature')
+      table.integer('infusions')
+      table.string('caffeine_level')
+      table.string('farmer')
+      table.string('farmer_img')
+
+  })
+    .createTable('comments', table => {
+      table.increments('id').primary()
+      table.integer('tea_id').unsigned()
+      table.foreign('tea_id').references('teas.id')
+      table.string('user_name')
+      table.string('user_message')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('teas')
+    .dropTableIfExists('comments')
+}
