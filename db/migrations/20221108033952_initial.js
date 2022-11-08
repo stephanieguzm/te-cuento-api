496d3239ac46d38988131e25ff80f94be8dbf159
Stephanie Guzman <sguzman247@gmail.com>
2022-11-08 09:00:14 -0600

    Change data type from string to text

--- db/migrations/20221108033952_initial.js
+++ db/migrations/20221108033952_initial.js
@@ -6,24 +6,24 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('teas', table => {
       table.increments('id').primary()
-      table.string('type')
-      table.string('name')
-      table.string('img')
-      table.string('origin')
+      table.text('type')
+      table.text('name')
+      table.text('img')
+      table.text('origin')
       table.text('description')
-      table.string('brew_time')
+      table.text('brew_time')
       table.integer('temperature')
       table.integer('infusions')
-      table.string('caffeine_level')
-      table.string('farmer')
-      table.string('farmer_img')
+      table.text('caffeine_level')
+      table.text('farmer')
+      table.text('farmer_img')
 
   })
     .createTable('comments', table => {
       table.increments('id').primary()
       table.integer('tea_id').unsigned()
       table.foreign('tea_id').references('teas.id')
-      table.string('user_name').notNullable()
+      table.text('user_name').notNullable()
       table.text('user_message').notNullable()
   })
 }
